61c643adf7a87ce7490c28443345021b8b770530
pratham591 <ps4955183@gmail.com>
2025-02-18 13:33:36 +0530

    new code

---
+++ push.sh
@@ -0,0 +1,3 @@
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519" git add .
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519" git commit -m "new code"
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519" git push origin pratham
